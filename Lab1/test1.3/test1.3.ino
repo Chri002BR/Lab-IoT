bc486e8d64fe5245fceb3feb7cbcbcf812f96b69
Chri002BR <forzachri02@gmail.com>
2026-03-10 22:46:33 +0100

    Fix ordine files

--- Lab1/test1.3/test1.3.ino
+++ Lab1/test1.3/test1.3.ino
@@ -1,11 +1,13 @@
-int pirPin = 7;      // Il pin a cui hai collegato l'OUT del sensore
+int pirPin = 4;      // Il pin a cui hai collegato l'OUT del sensore
+int ledPin = 5;     // Luce led per la rilevazione del movimento 
 int statoPIR = 0;    // Variabile per salvare lo stato del sensore
 
 void setup() {
   Serial.begin(9600);
   pinMode(pirPin, INPUT); // Impostiamo il pin del PIR come INPUT
+  pinMode(pirPin, OUTPUT); 
   Serial.println("Inizializzazione sensore in corso...");
-  delay(10000); // Il PIR ha bisogno di qualche secondo all'avvio per calibrarsi con la stanza
+  delay(30000); // Il PIR ha bisogno di qualche secondo all'avvio per calibrarsi con la stanza
   Serial.println("Sensore pronto!");
 }
 
@@ -14,10 +16,12 @@ void loop() {
 
   if (statoPIR == HIGH) {
     Serial.println("Movimento rilevato!");
+    digitalWrite(ledPin, HIGH);
     delay(1000); // Pausa per non intasare il monitor seriale
   } else {
     // Se è LOW, non c'è movimento
     Serial.println("Nessun movimento...");
+    digitalWrite(ledPin, LOW);
     delay(1000);
   }
 }
