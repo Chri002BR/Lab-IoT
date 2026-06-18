b779d0b0d9323743a5f9bfdb089673eecd046206
flaviobruno1998-source <flavio.bruno.1998@gmail.com>
2026-06-18 19:34:19 +0200

    Modifiche Es06

--- SW/Lab1/Clients/Es06/Es06.py
+++ SW/Lab1/Clients/Es06/Es06.py
@@ -1,49 +1,52 @@
-from SW.Lab1.CatalogClient import CatalogClient
 from pathlib import Path
+import sys
+import json
+
+# Aggiungo il percorso del progetto alla variabile di ambiente per poter importare il CatalogClient
+root_path = Path(__file__).resolve().parents[4]
+sys.path.append(str(root_path))
+
+from SW.Lab1.CatalogClient import CatalogClient
+
+## QUESTO SCRIPT HA LA FUNZIONE DI TEST PER IL CATALOG-CLIENT
 
 if __name__ == "__main__":
 
     # Leggo l'uri del catalog dal file di config
     uri_path = Path(__file__).parent / "config-uri-client.json"
         
-    # Recupero l'URL del catalogo dal file di configurazione, con fallback al default se non trovato
+    # Recupero l'URL del catalogo dal file di configurazione, prendo il default se non lo trovato
     try:
         with open(uri_path, "r") as f:
             config = json.load(f)
-        self.CATALOG_BASE_URL = config.get("uri_catalog", "http://localhost:9093/catalog")
-        print(f"Catalog URL: {self.CATALOG_BASE_URL}")
+        url_catalog = config.get("uri_catalog", "http://localhost:9093/catalog")
+        print(f"Catalog URL: {url_catalog}")
     except Exception: #se non trova su config, usa il default:
-        self.CATALOG_BASE_URL = "http://localhost:9093/catalog"
-        print(f"Impossibile leggere {uri_path}, uso default {self.CATALOG_BASE_URL}")
+        url_catalog = "http://localhost:9093/catalog"
+        print(f"Impossibile leggere {uri_path}, uso default {url_catalog}")
 
-    client = CatalogClient(self.CATALOG_BASE_URL)
+    client = CatalogClient(url_catalog)
 
     disp_prova = {
-        "id": "test-sensor-01",
+        "id": "test-sensor-group-14",
         "description": "Sensore di test per CatalogClient (es. 06)",
         "resources": ["temperature"],
     }
 
-    # Test di registrazione di un finto device
-    print("\n[POST] Registrazione Device...")
-    print(client.register_device(disp_prova))
+    print("\nRegistrazione device (POST):")
+    print(client.register_device(disp_prova)) # Test di registrazione di un finto device
 
-    # Test GET del catalogo completo e del broker
-    print("\n[GET] Intero Catalogo:")
-    print(client.get_catalog())
+    print("\nIntero Catalogo (GET):")
+    print(client.get_catalog()) # Test del catalogo e del broker
 
-    # Test GET del broker
-    print("\n[GET] Informazioni Broker MQTT:")
-    print(client.get_broker())
+    print("\nInfo Broker MQTT (GET):")
+    print(client.get_broker()) # Test GET del broker
 
-    # Test GET dei dispositivi
-    print("\n[GET] Tutti i Devices:")
-    print(client.get_devices())
+    print("\nStampa tutti i Devices (GET):")
+    print(client.get_devices()) #Test prende tutti i dispositivi registrati sul broker
 
-    # Test GET di un singolo dispositivo
-    print("\n[GET] Singolo Device (test-sensor-01):")
-    print(client.get_device("test-sensor-01"))
+    print("\nSingolo Device (test-sensor-group-14 in GET):")
+    print(client.get_device("test-sensor-group-14"))# Testsingolo dispositivo
 
-    # Test PUT di Refresh
-    print("\n[PUT] Refresh del Device...")
-    print(client.refresh_device("test-sensor-01"))
+    print("\nRefresh del Device (PUT):")
+    print(client.refresh_device("test-sensor-group-14")) #Test del refresh dispositivo
